eee6376c3007c8bdf3867c6bc747fdb7b70b1b12
Steven Silvester <steven.silvester@ieee.org>
2023-08-04 20:12:55 -0500

    install unzip

--- .evergreen/auth_aws/lib/ecs_hosted_test.sh
+++ .evergreen/auth_aws/lib/ecs_hosted_test.sh
@@ -9,6 +9,8 @@ set -ex
 env
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt-get update
+sudo apt-get install -y unzip
 unzip awscliv2.zip
 sudo ./aws/install
 aws sts get-caller-identity
