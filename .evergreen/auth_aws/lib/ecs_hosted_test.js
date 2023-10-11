baf89482438f5a820e693e8464607817f9109957
Steven Silvester <steven.silvester@ieee.org>
2023-10-10 20:26:42 -0500

    add debug info

--- .evergreen/auth_aws/lib/ecs_hosted_test.js
+++ .evergreen/auth_aws/lib/ecs_hosted_test.js
@@ -11,13 +11,19 @@ const AWS_ACCOUNT_ARN = "arn:aws:sts::557821124784:assumed-role/ecsTaskExecution
 const external = Mongo().getDB("$external");
 const admin = Mongo().getDB("admin");
 
+console.log('test1')
 admin.runCommand({createUser: "admin", pwd: "pwd", roles: ['root']});
+console.log('test2')
 admin.auth("admin", "pwd");
-
+console.log('test3')
 external.runCommand({createUser: AWS_ACCOUNT_ARN, roles:[{role: 'read', db: "aws"}]});
 
 // Try the auth function
+console.log('test4')
 const testConn = new Mongo();
+console.log('test5')
 const testExternal = testConn.getDB('$external');
+console.log('test6')
 assert(testExternal.auth({mechanism: 'MONGODB-AWS'}));
+console.log('test7')
 }());
