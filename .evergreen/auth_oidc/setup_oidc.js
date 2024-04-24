af73b92b08bcf260b82247af4ab8c8580003f407
Steven Silvester <steve.silvester@mongodb.com>
2024-04-23 19:36:44 -0500

    final setup

--- .evergreen/auth_oidc/setup_oidc.js
+++ .evergreen/auth_oidc/setup_oidc.js
@@ -6,20 +6,14 @@
 
 const adminUser = process.env['AZUREOIDC_USERNAME'] || 'bob';
 console.log("Setting up Admin User", adminUser);
-const admin = Mongo().getDB("admin");
+const conn = Mongo();
+const admin = conn.getDB("admin");
 assert(admin.auth(adminUser, "pwd123"));
 
 console.log("Setting up User");
-const authorizationPrefix = process.env['AZUREOIDC_AUTHPREFIX'] || 'test1';
-const authorizationClaim = process.env['AZUREOIDC_AUTHCLAIM'] || 'system:serviceaccount:default:oidc-test-sa';
-const role1Name = authorizationPrefix + '/' + authorizationClaim;
-const role2Name = 'test2/readWrite';
-
-// Add the roles.
-console.log('Adding role:', role1Name);
-admin.runCommand({createRole: role1Name, roles:[{role: 'readWrite', db: 'test'}], privileges: []});
-
-console.log('Adding role:', role2Name);
-admin.runCommand({createRole: role2Name, roles:[{role: 'readWrite', db: 'test'}], privileges: []});
+conn.getDB('$external').runCommand({
+    createUser: 'test1/system:serviceaccount:default:oidc-test-sa',
+    roles: [{role: 'readWriteAnyDatabase', db: 'admin'}]
+});
 
 }());
