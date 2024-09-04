3f15fef31de9553729f35d6751a5947c174adf9d
Steven Silvester <steve.silvester@mongodb.com>
2024-09-04 14:28:06 -0500

    fix rhel8-arm download

--- .evergreen/download-mongodb.sh
+++ .evergreen/download-mongodb.sh
@@ -177,8 +177,8 @@ get_mongodb_download_url_for ()
       linux-rhel-8*-aarch64)
          MONGODB_LATEST="http://downloads.10gen.com/linux/mongodb-linux-aarch64-enterprise-rhel82${DEBUG}-latest.tgz"
          MONGOSH="https://downloads.mongodb.com/compass/mongosh-${VERSION_MONGOSH}-linux-arm64.tgz"
-             MONGODB_RAPID="http://downloads.10gen.com/linux/mongodb-linux-aarch64-enterprise-rhel88${DEBUG}-${VERSION_RAPID}.tgz"
-             MONGODB_80="http://downloads.10gen.com/linux/mongodb-linux-aarch64-enterprise-rhel82${DEBUG}-${VERSION_80}.tgz"
+             MONGODB_RAPID="http://downloads.10gen.com/linux/mongodb-linux-aarch64-enterprise-rhel82${DEBUG}-${VERSION_RAPID}.tgz"
+             MONGODB_80="http://downloads.10gen.com/linux/mongodb-linux-aarch64-enterprise-rhel88${DEBUG}-${VERSION_80}.tgz"
              MONGODB_70="http://downloads.10gen.com/linux/mongodb-linux-aarch64-enterprise-rhel82${DEBUG}-${VERSION_70}.tgz"
              MONGODB_60="http://downloads.10gen.com/linux/mongodb-linux-aarch64-enterprise-rhel82${DEBUG}-${VERSION_60}.tgz"
              MONGODB_50="http://downloads.10gen.com/linux/mongodb-linux-aarch64-enterprise-rhel82${DEBUG}-${VERSION_50}.tgz"
