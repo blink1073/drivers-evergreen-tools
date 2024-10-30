a2f42c33d9f014b174b6e6fefa0cd7f8cf80085e
Steven Silvester <steve.silvester@mongodb.com>
2024-10-30 05:57:27 -0500

    debug

--- .evergreen/orchestration/handle_orchestration_file.py
+++ .evergreen/orchestration/handle_orchestration_file.py
@@ -2,6 +2,7 @@ import shutil
 import os
 from pathlib import Path
 import json
+import sys
 
 orig_file = Path(os.environ['ORCHESTRATION_FILE']).absolute()
 here = Path(__file__).absolute().parent
@@ -20,5 +21,8 @@ if os.environ.get('REQUIRE_API_VERSION'):
     data = json.loads(text)
     data['requireApiVersion'] = '1'
     new_file.write_text(json.dumps(data,indent=4))
+
+# TODO: remove before merging.
+print(new_file.read_text(), file=sys.stderr)
 
 print(str(new_file))
